90d8dfc6b35c5d5dc436653d3577e0dacdc1376e
annahico <annahico@gmail.com>
2024-06-27 09:35:10 +0200

    upgrate: change all project and imported mocks carpet prom github midu

--- shopping cart/src/App.jsx
+++ shopping cart/src/App.jsx
@@ -1,25 +1,10 @@
-import { Component } from 'react'
-import './App.css'
+import { Products } from "./components/Products";
+import productsData from "./mocks/products.json";
 
-
-class App extends Component {
-  state = {
-    productos: [
-      { name: 'Tomate', price: 1500, img: '/productos/tomate.jpg' },
-      { name: 'Arbeja', price: 2500, img: '/productos/arbeja.jpg' },
-      { name: 'Lechuga', price: 500, img: '/productos/lechuga.png' },
-    ]
-  }
-  render() {
-    return (
-      <div>
-        <p>Hola Mundo</p>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <Products products={productsData.products} />
+  );
 }
 
-
- 
-
-export default App
+export default App;
