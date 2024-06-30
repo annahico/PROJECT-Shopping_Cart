db1ad279c72ba241fd04f283da8f2f4215fdf211
annahico <annahico@gmail.com>
2024-06-30 14:24:15 +0200

    feat: footer.jsx created and improve App

--- shopping cart/src/App.jsx
+++ shopping cart/src/App.jsx
@@ -3,32 +3,42 @@ import { Header } from './components/Header.jsx';
 import { Products } from "./components/Products";
 import { products as initialProducts } from "./mocks/products.json";
 
-function App() {
-  // State for products and filters
-  const [products] = useState(initialProducts);
+function useFilters () {
+// State for products and filters
   const [filters, setFilters] = useState({
     category: 'all', // Filter by category
     minPrice: 0, // Filter by minimum price
-  });
+  })
 
   // Function to filter products based on selected filters
   const filterProducts = (products) => {
     return products.filter(product => {
       return (
         product.price > filters.minPrice && 
-        (filters.category === 'all' || product.category === filters.category)
-      );
-    });
-  };
+        (
+          filters.category === 'all' || 
+          product.category === filters.category
+        )
+      )
+    })
+  }
 
-  const filteredProducts = filterProducts(products);
-  
+  return { filterProducts, setFilters} 
+}
+
+
+function App() {
+  const [products] = useState(initialProducts)
+  const { filterProducts, setFilters} = useFilters ()
+  const filteredProducts = filterProducts(products)
+
   return (
     <>
       <Header changeFilters={setFilters}/>
       <Products products={filteredProducts} />
+      <Footer></Footer>
     </>
-  );
+  )
 }
 
 export default App;
