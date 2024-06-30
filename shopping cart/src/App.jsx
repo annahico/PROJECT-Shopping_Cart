e905039c0c95ce4ab377d8d6e59eca6a32deec24
annahico <annahico@gmail.com>
2024-06-30 21:27:24 +0200

    update: create cart context

--- shopping cart/src/App.jsx
+++ shopping cart/src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
+import { Cart } from './components/Cart.jsx'
 import { Footer } from './components/Footer.jsx'
 import { Header } from './components/Header.jsx'
 import { Products } from './components/Products.jsx'
 import { IS_DEVELOPMENT } from './config.js'
+import { CartProvider } from './context/cart.jsx'
 import { useFilters } from './hooks/useFilters.js'
 import { products as initialProducts } from './mocks/products.json'
 
+function App () {
+  const { filterProducts } = useFilters()
 
-function App() {
-  const [products] = useState(initialProducts)
-  const { filterProducts,  filters} = useFilters ()
-
-  const filteredProducts = filterProducts(products)
+  const filteredProducts = filterProducts(initialProducts)
 
   return (
-    <>
+    <CartProvider>
       <Header />
+      <Cart />
       <Products products={filteredProducts} />
-      { IS_DEVELOPMENT && <Footer filters={filters} />}
-    </>
+      {IS_DEVELOPMENT && <Footer />}
+    </CartProvider>
   )
 }
 
--- shopping cart/src/context/cart.jsx
+++ shopping cart/src/context/cart.jsx
@@ -6,18 +6,34 @@ export function CartProvider ({ children }) {
     const { cart, setCard } = useCartReducer([])
 
     const addToCart = => {
-setCart([...cart, product])
+const productInCartIndex = cart.findIndex(item => item.id === product.id)
+
+//if the product is already in the cart
+if (productInCartIndex > 0) {
+    const newCart = structuredClone(cart)
+    newCart[productInCartIndex].quantity += 1
+    return setCard(newCart)
+}
+
+// if the product in not in the cart
+SpeechRecognitionAlternative(prevState => ([
+    ...prevState,
+    {
+        ...product,
+        quantity: 1
+    }
+]))
     }
 
-    const claerCart = () => {
+
+    const clearCart = () => {
         setCard([])
     }
   
     return (
       <CartContext.Provider value={{
-        cart: state,
+        cart:
         addToCart,
-        removeFromCart,
         clearCart
       }}
       >
