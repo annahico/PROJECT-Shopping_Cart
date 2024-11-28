1ec7632e31185ab6eacd4064882a099ddde7f8f0
annahico <annahico@gmail.com>
2024-11-28 23:13:50 +0100

    feat: added some changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PROJECT-Shopping_Cart/'
+  base: '/PROJECT-Shopping_Cart/', // Nombre exacto del repositorio
+  build: {
+    outDir: 'dist', // Carpeta de salida para la construcción
+  },
+  resolve: {
+    alias: {
+      // Asegura que las rutas relativas sean compatibles
+      '/@': '/src',
+    },
+  },
+  server: {
+    open: true, // Abre automáticamente en el navegador al iniciar localmente
+    port: 3000, // Puerto para desarrollo local
+  },
 })
